70416f3202f1672e3dc56e5bd402cca8d1a6749a
ghost <jpirakas007@gmail.com>
2019-04-03 10:52:02 +0530

    first commit

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,8 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    
+
+    <div class="container">
+        <div class="row" id="app">
+            <h1>Chat Room</h1>
+        </div>
+    </div>
+
+<script src="{{asset('js/app.js')}}">
+</script> 
 </body>
 </html>
