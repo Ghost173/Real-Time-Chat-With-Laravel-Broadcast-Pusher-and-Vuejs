90c0fa505a550d126f7310ea3b8320d3fd70b1d1
ghost <jpirakas007@gmail.com>
2019-04-03 12:10:55 +0530

    scroll bar and improve design

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,15 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Chat Room</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <style>
+    .list-group {
+        overflow-y:scroll;
+        height:200px;
+    }
+    </style>
 </head>
 <body>
 
     <div class="container">
         <div class="row" id="app">
-            <h1>Chat Room</h1>
+            <div class="offset-4 col-4">
+             <li class="list-group-item active">Chat Rooms</li>
+                <ul class="list-group ">
+                   
+                        <message v-for="value in chat.message">
+                        @{{value}}
+                        </message> 
+                </ul>
+                    <input type="text" class="form-control" placeholder="type your message here ...." v-model='message'
+                    @keyup.enter='send'>
+            </div>
+
+            
         </div>
     </div>
 
